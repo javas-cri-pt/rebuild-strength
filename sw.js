50bfade89a834faab2064ff26d7c9c9877010775
Cri <74323315+javas-cri-pt@users.noreply.github.com>
2026-06-09 22:56:10 +0200

    feat: PWA support (manifest, service worker, icons)

---
+++ sw.js
@@ -0,0 +1,25 @@
+var CACHE = 'rebuild-strength-v1';
+var ASSETS = [
+  '.', 'index.html', 'manifest.json',
+  'icons/icon-192.png', 'icons/icon-512.png', 'icons/apple-touch-icon.png'
+];
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }).then(function(){ return self.skipWaiting(); }));
+});
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(k){ if(k!==CACHE) return caches.delete(k); }));
+  }).then(function(){ return self.clients.claim(); }));
+});
+self.addEventListener('fetch', function(e){
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      return hit || fetch(e.request).then(function(res){
+        var copy=res.clone();
+        caches.open(CACHE).then(function(c){ try{ c.put(e.request, copy); }catch(_){} });
+        return res;
+      }).catch(function(){ return caches.match('index.html'); });
+    })
+  );
+});
