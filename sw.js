4caa1ba8c60e6e1d03a6e7bcf2cd14100893bdef
Cri <74323315+javas-cri-pt@users.noreply.github.com>
2026-06-12 13:41:34 +0200

    chore: bump SW cache to v3 (ships dashboard + XSS fix)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'rebuild-strength-v2';
+var CACHE = 'rebuild-strength-v3';
 var ASSETS = [
   '.', 'index.html', 'manifest.json',
   'icons/icon-192.png', 'icons/icon-512.png', 'icons/apple-touch-icon.png'
